02b58cda2332cdc2a18bd5d6e8569894cdc6348b
MatteoOliva <oliva.matteo98@outlook.com>
2024-02-29 16:44:35 +0100

    feat: stampato in console il contenuto del file get-list-php che abbiam convertito prima in array per php dal file json

--- frontend/main.js
+++ frontend/main.js
@@ -6,5 +6,17 @@ const app = createApp({
             todoList: ['disciplina', 'disciplina', 'disciplina', 'disciplina']
         };
     },
+
+    methods: {
+        fetchTodoList() {
+            axios.get('http://localhost/php-todo-list-json/backend/api/get-list.php').then((response) => {
+                this.todoList = response.data;
+                console.log(response.data);
+            });
+        },
+    },
+    mounted() {
+        this.fetchTodoList();
+    },
 });
 app.mount('#app');
